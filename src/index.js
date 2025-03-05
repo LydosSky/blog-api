66328c647f6d4034a35a7ebeb8812484975fbac0
Lydos <lydossky@gmail.com>
2025-03-05 19:42:47 +0300

    chore: install morgan and setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import morgan from 'morgan';
+
 import routes from './routes';
 import middlewares from './middlewares';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/user', routes.user);
 
@@ -13,4 +16,4 @@ app.use(middlewares.errors.notFound);
 app.use(middlewares.errors.serverError);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API PORT:${PORT}`));
+app.listen(PORT);
