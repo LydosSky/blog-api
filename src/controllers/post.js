c2a365a0bd99bf736f7b0370dd2ac1d2cb3e8eef
Lydos <lydossky@gmail.com>
2025-03-06 12:23:40 +0300

    feat: post controller

---
+++ src/controllers/post.js
@@ -0,0 +1,86 @@
+/**
+ * Post controller.
+ *
+ * @description Functions to handle Post related operations.
+ * @module controllers/post
+ */
+
+import expressAsyncHandler from 'express-async-handler';
+import models from '../models';
+
+/**
+ * Handles GETing all of the posts.
+ * note: Function is wrapped with `expressAsyncHandler` for
+ * error handling.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @returns {Promise<void>} - Promise resolves when the response is sent.
+ */
+const getPost = expressAsyncHandler(function (req, res) {
+  return models.post.getPosts().then((posts) => res.json(posts));
+});
+
+/**
+ * Handles GETting a post with ID.
+ * note: Function is wrapped with `expressAsyncHandler` for
+ * error handling.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @returns {Promise<void>} - Promise resolves when the response is sent.
+ */
+const getPostById = expressAsyncHandler(function (req, res) {
+  return models.post.getPostById(req.params.id).then((post) => res.json(post));
+});
+
+/**
+ * Handles POSTing a post.
+ * note: Function is wrapped with `expressAsyncHandler` for
+ * error handling.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @returns {Promise<void>} - Promise resolves when the response is sent.
+ */
+const createPost = expressAsyncHandler(function (req, res) {
+  return models.post
+    .createPost({
+      title: req.body.title,
+      content: req.body.content,
+      userId: req.body.userId,
+    })
+    .then((post) => res.json(post));
+});
+
+/**
+ * Handlers PUTing a post.
+ * note: Function is wrapped with `expressAsyncHandler` for
+ * error handling.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @returns {Promise<void>} - Promise resolves when the response is sent.
+ */
+const updatePost = expressAsyncHandler(function (req, res) {
+  return models.post
+    .updatePost({
+      id: req.params.id,
+      title: req.body.title,
+      content: req.body.content,
+    })
+    .then((post) => res.json(post));
+});
+
+/**
+ * Handlers DELETing a post.
+ * note: Function is wrapped with `expressAsyncHandler` for
+ * error handling.
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @returns {Promise<void>} - Promise resolves when the response is sent.
+ */
+const deletePost = expressAsyncHandler(function (req, res) {
+  return models.post.deletePost(req.params.id).then((post) => res.json(post));
+});
