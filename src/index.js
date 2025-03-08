a35080234b638214d68919315ba952b6d40a64a4
Lydos <lydossky@gmail.com>
2025-03-08 22:00:08 +0300

    feat: cors middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 import morgan from 'morgan';
-
+import cors from 'cors';
 import routes from './routes';
 import middlewares from './middlewares';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
