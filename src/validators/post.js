b3f952624cf5ba97e50be61b74213270a8d3bd73
Lydos <lydossky@gmail.com>
2025-03-18 22:23:41 +0300

    feat: post validators

---
+++ src/validators/post.js
@@ -0,0 +1,28 @@
+import { body, param } from 'express-validator';
+
+const createValidator = [
+  body('title')
+    .trim()
+    .notEmpty()
+    .withMessage('Post must have a title')
+    .length({ max: 70 })
+    .withMessage('Title must be at most 70 characters long.'),
+  body('content').trim().notEmpty().withMessage('Post must have a content'),
+];
+
+const updateValidator = [
+  param('id').trim().notEmpty().withMessage('Id of the post missing.'),
+  body('title')
+    .trim()
+    .notEmpty()
+    .withMessage('Post must have a title')
+    .length({ max: 70 })
+    .withMessage('Title must be at most 70 characters long.'),
+  body('content').trim().notEmpty().withMessage('Post must have a content'),
+];
+
+const deleteValidator = [
+  param('id').trim().notEmpty().withMessage('Id of the post missing.'),
+];
+
+export default { createValidator, updateValidator, deleteValidator };
