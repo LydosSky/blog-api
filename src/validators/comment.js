1c8ce7e627506635bc943a85a792a9c9ba738fee
Lydos <lydossky@gmail.com>
2025-03-19 21:23:40 +0300

    refactor: existence check and wording

--- src/validators/comment.js
+++ src/validators/comment.js
@@ -1,22 +1,43 @@
 import { body, param } from 'express-validator';
 
 const createValidator = [
-  body('content').trim().notEmpty().withMessage('Content is required.'),
-  body('postId')
+  body('content')
+    .exists()
+    .withMessage('Content is required.')
     .trim()
     .notEmpty()
-    .withMessage('Post ID is required.')
+    .withMessage('Content cannot be empty.'),
+  body('postId')
+    .exists()
+    .withMessage('Post ID is required')
+    .trim()
+    .notEmpty()
+    .withMessage('Post ID cannot be empty.')
     .isUUID()
     .withMessage('Post ID is UUID.'),
 ];
 
 const updateValidator = [
-  param('id').trim().notEmpty().withMessage('Comment ID is required'),
+  param('id')
+    .exists()
+    .withMessage('ID parameter is required.')
+    .trim()
+    .notEmpty()
+    .withMessage('Comment ID cannot be empty.')
+    .isUUID()
+    .withMessage('Comment ID is UUID.'),
   body('content').trim().notEmpty().withMessage('Content is required.'),
 ];
 
 const deleteValidator = [
-  param('id').trim().notEmpty().withMessage('Comment ID is required'),
+  param('id')
+    .exists()
+    .withMessage('ID parameter is required.')
+    .trim()
+    .notEmpty()
+    .withMessage('Comment ID cannt be empty.')
+    .isUUID()
+    .withMessage('Comment ID is UUID.'),
 ];
 
 export default { createValidator, updateValidator, deleteValidator };
